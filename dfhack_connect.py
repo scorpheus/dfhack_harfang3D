46ad9b439bc14ff8b8675ce2fc415763410e8db4
tsimonnet <scorpheus.mr@gmail.com>
2015-06-25 11:14:01 +0200

    using shared memory and some tricks, from 0.01s to 0.003sec

--- dfhack_connect.py
+++ dfhack_connect.py
@@ -189,6 +189,38 @@ def GetListUnits():
 
 	return out
 
+import numpy as np
+import mmap
+from struct import *
+
+shm_block = mmap.mmap(0, 1+3*4+16*16*2+16*16+16*16, "Local\\df_block") # You should "open" the memory map file instead of attempting to create it..
+shm_pos = mmap.mmap(0, 1+3*4, "Local\\df_pos")
+
+# initialize the shared memory
+shm_block[0] = 0
+shm_pos[0] = 0
+
+def GetBlockMemory(pos):
+	block = None
+	block_pos = None
+	block_flow_size = None
+	block_liquid_type = None
+	# if block available
+	if shm_block[0] != 0:
+		block = np.frombuffer(shm_block[(1+3*4):(1+3*4+16*16*2)], dtype=np.uint16)
+		block_flow_size = np.frombuffer(shm_block[(1+3*4+16*16*2):(1+3*4+16*16*2+16*16)], dtype=np.uint8)
+		block_liquid_type = np.frombuffer(shm_block[(1+3*4+16*16*2+16*16):(1+3*4+16*16*2+16*16+16*16)], dtype=np.uint8)
+		block_pos = np.frombuffer(shm_block[1:(1+3*4)], dtype=np.uint32)
+		shm_block[0] = 0
+
+	# if no pos and no block
+	# send another pos
+	if pos is not None and shm_pos[0] == 0 and shm_block[0] == 0:
+		shm_pos.seek(0)
+		packed_data = pack('=BIII', 3, int(pos.x), int(pos.y), int(pos.z))
+		shm_pos.write(packed_data)
+
+	return block_pos, block, block_flow_size, block_liquid_type
 
 def GetBlock(pos):
 
